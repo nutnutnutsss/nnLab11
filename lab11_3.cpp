433356eb28868e1b2e2c491f865ff92917a2756a
NutNatcha <natcha_me@cmu.ac.th>
2021-12-17 18:29:08 +0700

    lab11_3 complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,39 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int count;
+    float sum;
+    ifstream source("score.txt");
+    string textline;
+
+    while(getline(source,textline)){
+        sum += atof(textline.c_str());
+        count++;
+    }
+
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << sum/count << '\n';
+    source.close();
+
+    source.open("score.txt");
+    float mean = sum/count;
+    sum = 0;
+    count = 0;
+
+    while(getline(source,textline)){
+        sum += pow((atof(textline.c_str())-mean),2);
+        count++;
+    }
+
+    float deviation = sqrt(sum/count);
+
+    cout << "Standard deviation = " << deviation << "\n" ;
+    source.close();
+
+    return 0;
+}
